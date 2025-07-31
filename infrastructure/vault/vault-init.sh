0204d942924927b6b38adb8a693ea4a7d2c0510f
Никита <nikita.malygin2017@mail.ru>
2025-08-01 01:16:16 +0300

    Changes for some services infrastructure

--- infrastructure/vault/vault-init.sh
+++ infrastructure/vault/vault-init.sh
@@ -7,17 +7,6 @@ VAULT_KEYS_FILE=".vault-keys"
 export VAULT_SKIP_VERIFY=true
 export VAULT_ADDR=$VAULT_ADDR
 
-echo "Ожидаем запуска Vault..."
-# until vault status &> /dev/null; do
-#   sleep 1
-# done
-
-# if ! vault status | grep -q 'Initialized.*true'; then
 echo "Vault не инициализирован. Инициализация..."
 vault operator init -key-shares=5 -key-threshold=3 > $VAULT_KEYS_FILE
 echo "Vault инициализирован. Ключи сохранены в $VAULT_KEYS_FILE"
-# fi
-
-# if vault status | grep -q 'Sealed.*true'; then
-
-# fi
